5a4b2630ebe715046ed22c82951ae212bceb2a6c
Manel Roca <manel.roca@meetinglawyers.com>
2023-01-05 15:03:59 +0100

    feat: finished simonprickett.dev echo tutorial and updated to new Swift version

---
+++ MeetingLawyers/src/ios/MeetingLawyers.swift
@@ -0,0 +1,45 @@
+@objc(MeetingLawyers) class MeetingLawyers : CDVPlugin {
+    @objc(echo:)
+    func echo(_ command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        
+        let msg = command.arguments[0] as? String ?? ""
+        
+        if msg.characters.count > 0 {
+            /* UIAlertController is iOS 8 or newer only. */
+            let toastController: UIAlertController =
+            UIAlertController(
+                title: "",
+                message: msg,
+                preferredStyle: .alert
+            )
+            
+
+            self.viewController?.present(
+                toastController,
+                animated: true,
+                completion: nil
+            )
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0)
+            {
+                toastController.dismiss(
+                    animated: true,
+                    completion: nil
+                )
+            }
+            
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: msg
+            )
+        }
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+}
--- MeetingLawyers/www/MeetingLawyers.js
+++ MeetingLawyers/www/MeetingLawyers.js
@@ -1,5 +1,17 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'MeetingLawyers', 'coolMethod', [arg0]);
-};
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'MeetingLawyers', 'coolMethod', [arg0]);
+// };
+
+exports.echo = function(arg0, success, error) {
+    exec(success, error, "MeetingLawyers", "echo", [arg0]);
+  };
+  
+  exports.echojs = function(arg0, success, error) {
+    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+      success(arg0);
+    } else {
+      error('Empty message!');
+    }
+  };
